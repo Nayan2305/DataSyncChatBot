60809f6b8288b8fdd2299ba939182344277d9b52
Nayan2305 <thombre23nayan@gmail.com>
2023-08-24 21:16:33 +0530

    Skeleton done

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,5 +1,7 @@
-export {default as AddMachine} from './AddMachine';
-export {default as Home} from './Home';
-export {default as AddUser} from './AddUser';
-export {default as Login} from './Login';
-export {default as Register} from './Register';
+export { default as AddMachine } from "./AddMachine";
+export { default as Home } from "./Home";
+export { default as AddUser } from "./AddUser";
+export { default as Login } from "./Login";
+export { default as Register } from "./Register";
+export { default as PageNotFound } from "./PageNotFound";
+export { default as Contact } from "./Contact";
--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,28 @@
-// import React from 'react';
-// import ReactDOM from "react-dom/client";
-// import './index.css';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { Home,AddMachine,AddUser,Login,Register } from "./Pages";
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home,AddMachine,AddUser,Login,Register, PageNotFound, Contact } from "./Pages";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <BrowserRouter>
-//   <Provider store={store}>
-//     <Routes>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Provider store={store}>
+    <Routes>
 
-//       <Route path="/Home" element={<Home />} />
-//       <Route path="/AddUser" element={<AddUser />} />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/AddUser" element={<AddUser />} />
 
-//       <Route path="/Login" element={<Login />} />
-//       <Route path="/Contact" element={<Contact />} />        
-//       <Route path="/Register" element={<Register />} />
-//       <Route path="/AddMachine" element={ <AddMachine/>} />
-//       <Route path="/PageNotFound" element={ <PageNotFound />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Contact" element={<Contact />} />        
+      <Route path="/Register" element={<Register />} />
+      <Route path="/AddMachine" element={ <AddMachine/>} />
+      <Route path="/PageNotFound" element={ <PageNotFound />} />
 
-//     </Routes>
-//   </Provider>
-// </BrowserRouter>
-// );
+    </Routes>
+  </Provider>
+</BrowserRouter>
+);
 
