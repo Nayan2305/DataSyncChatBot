17c2b1fcc210e518dadf8aaf068e271b02ea2df6
Nayan2305 <thombre23nayan@gmail.com>
2023-08-28 20:18:49 +0530

    navbar added to AddMachine

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.css";
+import image from './image.jpeg';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-left">
+      <img src={image} alt="IMAGE" width="100" height="50"/>
+        {/* <div className="logo">Sunshine Powertronics</div> */}
+      </div>
+      <div className="navbar-middle">
+        <ul className="navbar-menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Contact Us</a></li>
+          <li><a href="#">About Us</a></li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <button to='/Login' className="login-button">Login / Register</button>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- client/src/Pages/AddMachine.jsx
+++ client/src/Pages/AddMachine.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {Navbar} from '../Components/index.js'
 const AddMachine = () => {
   return (
     <div>
-      
+       <Navbar/>
+       
     </div>
   )
 }
