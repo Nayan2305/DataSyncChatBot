097706313f264aaf133fdd7bb8a3ccabcb5c9a90
Tanmay Deshpande <tanmaydeshpande1588@gmail.com>
2023-09-03 00:05:44 +0530

    User Profile changed

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -4,5 +4,5 @@ export { default as AddUser } from "./AddUser";
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
 export { default as PageNotFound } from "./PageNotFound";
-export { default as Contact } from "./Contact";
+export { default as Contact} from "./Contact";
 export { default as User } from "./user";
